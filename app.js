c597e84fd0b845b265e584850ef7f1e3c95f4591
Peter Robert <pwambui93@gmail.com>
2021-07-01 12:23:03 +0300

    Set up complete

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-//=== initialize the app
+//=== Initialize the app.
 const app = express();
 
-//=== 
+//=== Connect to database.
+mongoose.connect('mongodb://localhost/storefront', (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('database connection succesful')
+})
+
+// ====Home route
+
+app.get('/', (req, res) => {
+    res.send("welcome to home")
+})
 
 
-//=== server
+//=== Server.
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
-   if(err){
-       console.log(err)
-   }
-   console.log("listening to port" + PORT)
+    if (err) {
+        console.log(err)
+    }
+    console.log("listening to port " + PORT)
 });
 
 
