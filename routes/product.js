0dd077f3310201c99ac4cbda568523cebd04653b
Peter Robert <pwambui93@gmail.com>
2021-07-07 09:40:40 +0300

    Create product route

---
+++ routes/product.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const Router = express.Router();
+// == Custom modules;
+const { Product, productValidation } = require('../models/product');
+
+Router.get('/', async (req, res) => {
+    try {
+        const results = await Product.find();
+        if (results.length < 1) return res.status(200).send('There are no products yet');
+        res.status(200).send(results)
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+module.exports = Router
