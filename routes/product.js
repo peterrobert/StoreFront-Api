9151821832b90f55ae39c63b856acf508ac2232a
Peter Robert <pwambui93@gmail.com>
2021-07-07 10:23:47 +0300

    Create product route

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Router = express.Router();
 // == Custom modules;
 const { Product, productValidation } = require('../models/product');
+const {Category} = require('../models/category')
 
 Router.get('/', async (req, res) => {
     try {
@@ -14,10 +15,30 @@ Router.get('/', async (req, res) => {
 })
 
 Router.post('/:categoryID', (req, res) => {
-    
-      
-    // ==== VALIDATION
 
+    const createProduct = async (obj) => {
+        const category = await Category.findById(obj.categoryID)
+        const product = new Product({
+            name: obj.name,
+            price: obj.price,
+            category: category
+        });
+        const data = await product.save();
+        res.status(201).send(data)
+    }
+
+    // ==== VALIDATION
+    let results = productValidation({
+        name: req.body.name,
+        price: req.body.price,
+        categoryID: req.params.categoryID
+    })
+
+    results.then((data) => {
+        createProduct(data);
+    }).catch((err) => {
+        res.status(404).send(err.details[0].message)
+    })
 })
 
 
